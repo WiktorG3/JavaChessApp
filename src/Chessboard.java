42755b2f0c3961a8f3345f569543d1eef0141168
WiktorG3 <wiktor.gronostaj03@gmail.com>
2024-09-16 23:25:43 +0200

    Defining chess Pieces and other Classes

---
+++ src/Chessboard.java
@@ -0,0 +1,12 @@
+public class Chessboard {
+    private Piece[][] board;
+  
+    public Chessboard() {
+        this.board = new Piece[8][8]; // 8x8 chessboard
+        setupPieces();
+    }
+  
+    private void setupPieces() {
+        //
+    }
+  }
